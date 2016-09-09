0794d03072bb9d2c7eee997312320e21a6e128cb
Coookiez <insanecookiez22@gmail.com>
2016-09-09 19:06:02 +0200

    Final version

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public GameObject defenderPrefab;
+	public static GameObject selectedDefender;
+
+	private Text costText;
+	private Button[] buttonArray;
+
+	// Use this for initialization
+	void Start () {
+		buttonArray = GameObject.FindObjectsOfType<Button> ();
+
+		costText = GetComponentInChildren<Text> ();
+		if (!costText) {
+			Debug.LogWarning (name + " has no cost text");
+		}
+
+		costText.text = defenderPrefab.GetComponent<Defender>().starCost.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		foreach (Button thisButton in buttonArray) {
+			thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+		}
+		GetComponent<SpriteRenderer>().color = Color.white;
+		selectedDefender = defenderPrefab;
+	}
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile, gun;
+
+	private GameObject projectileParent;
+	private Animator animator;
+	private Spawner myLaneSpawner;
+	void Start() {
+		projectileParent = GameObject.Find ("Projectiles");
+		animator = GameObject.FindObjectOfType<Animator> ();
+		if (!projectileParent) {
+			projectileParent = new GameObject("Projectiles");
+		}
+
+		SetMyLaneSpawner ();
+		print (myLaneSpawner);
+	}
+
+	void Update() {
+		if (IsAttackerAheadInLane ()) {
+			animator.SetBool ("isAttacking", true);
+		} else {
+			animator.SetBool ("isAttacking", false);
+		}
+	}
+
+	// Look through all spawners and set myLaneSpawner if found
+	void SetMyLaneSpawner() {
+		Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner> ();
+
+		foreach (Spawner spawner in spawnerArray) {
+			if (spawner.transform.position.y == transform.position.y) {
+				myLaneSpawner = spawner;
+				return;
+			}
+
+			Debug.LogError (name + " can't find spawner in lane");
+		}
+	}
+	bool IsAttackerAheadInLane() {
+		// Exit if no attackers in lane;
+		if (myLaneSpawner.transform.childCount <= 0) {
+			return false;
+		}
+
+		// If there are attackers
+		foreach (Transform attacker in myLaneSpawner.transform) {
+			if (attacker.transform.position.x > transform.position.x) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	private void Fire() {
+		GameObject newProjectile = Instantiate (projectile) as GameObject;
+		newProjectile.transform.parent = projectileParent.transform;
+		newProjectile.transform.position = gun.transform.position;
+	}
+}
